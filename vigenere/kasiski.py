4ed6ce80829b595998726c640f5086d61f932beb
Ion Dodon <iondodon2@gmail.com>
2022-01-31 18:27:55 +0200

    kasiski

--- vigenere/kasiski.py
+++ vigenere/kasiski.py
@@ -1,93 +1,87 @@
-# kasiski
-#
+cryptotext = "ABAJFGABASJD"
+
+# find equal substrings
+def find_equal_substrings(cryptotext):
+    # create a list of all substrings
+    substrings = []
+    for i in range(len(cryptotext)):
+        for j in range(len(cryptotext)):
+            substrings.append(cryptotext[i:j])
+
+    # create a dictionary of substrings and their occurences
+    substring_dict = {}
+    for substring in substrings:
+        if substring in substring_dict:
+            substring_dict[substring] += 1
+        else:
+            substring_dict[substring] = 1
+    
+    # create a list of substrings that occur more than once
+    equal_substrings = []
+    for substring in substring_dict:
+        if substring_dict[substring] > 1:
+            equal_substrings.append(substring)
+
+    return (substring_dict, equal_substrings)
 
 
-import argparse
-import os
-import re
-import sys
-import time
-from collections import Counter
-from itertools import chain
-from typing import List, Tuple
+def max_frequency_sustring(substring_dict):
+    max_frequency = 0
+    for substring in substring_dict:
+        if substring_dict[substring] > max_frequency:
+            max_frequency = substring_dict[substring]
+    return max_frequency
 
 
-def get_args() -> argparse.Namespace:
-    """
-    Get the command line arguments
-    """
-    parser = argparse.ArgumentParser(
-        description="Kasiski test",
-        formatter_class=argparse.ArgumentDefaultsHelpFormatter
-    )
+def get_longest_substring(equal_substrings):
+    longest_substring = ""
+    for substring in equal_substrings:
+        if len(substring) > len(longest_substring):
+            longest_substring = substring
 
-    parser.add_argument(
-        "--input",
-        type=str,
-        default="input.txt",
-        help="Input file"
-    )
-
-    parser.add_argument(
-        "--output",
-        type=str,
-        default="output.txt",
-        help="Output file"
-    )
-
-    return parser.parse_args()
+    return longest_substring
 
 
-def get_message(filename: str) -> str:
-    """
-    Get the message from the file
-    """
-    with open(filename, "r") as f:
-        message = f.read()
+def get_indexes(equal_substrings):
+    substrings_indexes = {}
+    for substring in equal_substrings:
+        indexes = []
+        for i in range(len(cryptotext)):
+            if cryptotext[i:i+len(substring)] == substring:
+                indexes.append(i)
+        substrings_indexes[substring] = indexes
+        
+    return substrings_indexes
 
-    return message
+def main():
+    print(cryptotext)
 
+    frequencies, equal_substrings = find_equal_substrings(cryptotext)
+    
+    # longest substring
+    longest_substring = get_longest_substring(equal_substrings)
 
-def get_key_length(message: str) -> int:
-    """
-    Get the key length
-    """
-    key_length = 0
-    for i in range(1, len(message)):
-        for j in range(0, len(message) - i):
-            if message[j:j + i] * (len(message) // i) == message:
-                key_length = i
-                break
+    # frequency of longest substring
+    frequency_longest_substring = frequencies[longest_substring] 
 
-    return key_length
+    print("Longest substring:", longest_substring)
+    print("Frequency of longest substring:", frequency_longest_substring)
 
+    # indexes of longest substring
+    indexes = get_indexes(equal_substrings)
+    print("Indexes of longest substring:", indexes)
 
-def get_key(message: str, key_length: int) -> str:
-    """
-    Get the key
-    """
-    key = ""
-    for i in range(key_length):
-        key += chr(ord("A") + Counter(message[i::key_length]).most_common(1)[0][0])
+    for substring in equal_substrings:
+        # substring length
+        substring_length = len(substring)
+        if substring_length == 0:
+            continue
+        # if all indexes divide by substring length without remainder
+        if all(i == 0 or i % substring_length == 0 for i in indexes[substring]):
+            print('\n')
+            print("Substring:", substring)
+            print("Indexes:", indexes[substring])
+            print("Key Length:", substring_length)
 
-    return key
-
-
-def get_key_from_file(filename: str) -> str:
-    """
-    Get the key from the file
-    """
-    with open(filename, "r") as f:
-        key = f.read()
-
-    return key
-
-
-def get_key_from_message(message: str) -> str:
-    """
-    Get the key from the message
-    """
-    key_length = get_key_length(message)
-    key = get_key(message, key_length)
-
-    return key
+if __name__ == "__main__":
+    main()
